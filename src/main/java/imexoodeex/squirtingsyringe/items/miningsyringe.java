3581b7c14d93b702b6f2802d8cfc07fea9d08148
imexoodeex <odeex722@gmail.com>
2022-06-19 14:30:29 +0200

    port to 1.19

--- src/main/java/imexoodeex/squirtingsyringe/items/miningsyringe.java
+++ src/main/java/imexoodeex/squirtingsyringe/items/miningsyringe.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -28,7 +27,7 @@ public class miningsyringe extends Item {
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(new TranslatableText(getClass().getSimpleName()));
+        tooltip.add(Text.translatable(getClass().getSimpleName()));
     }
 
     @Override
--- src/main/java/imexoodeex/squirtingsyringe/items/warriorsyringe.java
+++ src/main/java/imexoodeex/squirtingsyringe/items/warriorsyringe.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -29,7 +28,7 @@ public class warriorsyringe extends Item {
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(new TranslatableText(getClass().getSimpleName()));
+        tooltip.add(Text.translatable(getClass().getSimpleName()));
     }
 
     @Override
